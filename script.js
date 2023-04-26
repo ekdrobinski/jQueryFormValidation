933ac6b5cc462ea4f08c255bee1afa4aca34e910
edrobi <ekdrobinski@gmail.com>
2023-04-25 19:16:21 -0500

    communicated with accountability group but still couldn't get red to work

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
+
 $(document).ready(function() {
     $('#form').submit(function(event) {
       event.preventDefault(); // prevent form submission
   
       // create variables for each input
-      var name = $('#name');
-      var email = $('#email');
-      var phoneNumber = $('#phone');
+      var name = $("#name");
+      var email = $("#email");
+      var phoneNumber = $("#phone");
   
       // reate an array of required inputs
       var required = [name, email, phoneNumber];
   
       // loop through required inputs and check if they're filled out
       for (var i = 0; i < required.length; i++) {
-        if (required[i].val() === '') { //if the value is an empty string
-          $('#message').text('Please Fill Out Required Fields').addClass('warning');//populate required fields
-          required[i].parent().addClass('warning'); //add the warning class
+        if (required[i].val() === "") { //if the value is an empty string
+          $('#message').text("Please Fill Out Required Fields").addClass("warning");//populate required fields
+          required[i].parent().addClass("warning"); //add the warning class
         } else {
-          required[i].parent().removeClass('warning');
+          // Remove the warning class if the text field has a an input value
+          $("label").eq(i).removeClass("warning");
         }
       }
   
-      //check if any labels have the warning class
-      if ($('label').hasClass('warning')) {//attach the warning class to the array's items label
-        //verify that no labels have the warning class
-        return false;
-      } else {
-        // if no labels have the warning class take the form from the DOM and manipulate the h2 to say Thanks for your feedback
-        $('#form').html('<h2>Thanks for your feedback!</h2>');
-        return true;
-      }
+   // Check if any fields are still missing values
+   if ($(".warning").length === 0) {
+    // Remove the form and display a success message
+    $("#form").remove(); 
+    $("#pre-form h2").text("Thanks for your feedback!");
+  }
     });
   });
   
